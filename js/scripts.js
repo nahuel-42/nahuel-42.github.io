1ec23a8abc44e9d008fba81df88ee9f46397ba9f
nahuel-42 <nievas.nahuel.1998@gmail.com>
2024-03-26 15:19:11 -0300

    Modificando vista empresa, algunos cambios en la estructura

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    console.log("entro")
+    var image = document.getElementById('division1');
+    console.log(image)
+    image.classList.add('limpiado');
+});
