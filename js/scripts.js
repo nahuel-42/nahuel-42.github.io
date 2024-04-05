a4bd23e2bd03947599c8aa6d8618ff50c01f1c85
nahuel-42 <nievas.nahuel.1998@gmail.com>
2024-04-04 22:28:35 -0300

    Cambio blureado animacion

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    var images = document.getElementsByClassName('division1');
-    for (var i = 0; i < images.length; i++) {
-        images[i].classList.add('limpiado');
-    }
-});
-
