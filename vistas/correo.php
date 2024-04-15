524c0dc5452bd907f3f7933a406d350f6cbde7aa
nahuel-42 <nievas.nahuel.1998@gmail.com>
2024-04-15 09:37:30 -0300

    cambios en email y wpp

--- vistas/correo.php
+++ vistas/correo.php
@@ -1,10 +1,11 @@
 <?php
     if(isset($_POST['enviar'])){
         $email = $_POST['Email'];
-        $asunto = "Correo de prueba";
+        $asunto = "Esto es un correo de prueba, en caso de funcionar lo modificaremos para que quede mejor";
         $mensaje = $_POST['message'];
+        $destinatario = "geninfernando@hotmail.com";
         $cabeceras = 'From: ' . $_POST['Email'] . "\r\n" ;
-        $cabeceras .= 'Reply-To: noreply@example.com' . "\r\n" ;
+        $cabeceras .= 'Reply-To: geninfernando@hotmail.com' . "\r\n" ;
         if(mail($destinatario, $asunto, $mensaje, $cabeceras)){
             header('Location: contacto.html?correo=enviado');
         } else {
