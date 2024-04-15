d0649ad9c0d4825e7f1b8964c1dfe12c341de58d
nahuel-42 <nievas.nahuel.1998@gmail.com>
2024-04-15 00:24:55 -0300

    agregado el email

---
+++ vistas/correo.php
@@ -0,0 +1,17 @@
+<?php
+    if(isset($_POST['enviar'])){
+        $email = $_POST['Email'];
+        $asunto = "Correo de prueba";
+        $mensaje = $_POST['message'];
+        $cabeceras = 'From: ' . $_POST['Email'] . "\r\n" ;
+        $cabeceras .= 'Reply-To: noreply@example.com' . "\r\n" ;
+        if(mail($destinatario, $asunto, $mensaje, $cabeceras)){
+            header('Location: contacto.html?correo=enviado');
+        } else {
+            header('Location: contacto.html?correo=noenviado');
+        }
+        
+        exit;
+    }
+?>
+        
