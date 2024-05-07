183daa280a805a1c06d334f5f5d93f365f9d979f
nahuel-42 <nievas.nahuel.1998@gmail.com>
2024-05-07 17:55:56 -0300

    cambios ortograficos

--- vistas/correo.php
+++ vistas/correo.php
@@ -3,9 +3,9 @@
         $email = $_POST['Email'];
         $asunto = "Esto es un correo de prueba, en caso de funcionar lo modificaremos para que quede mejor";
         $mensaje = $_POST['message'];
-        $destinatario = "geninfernando@hotmail.com";
+        $destinatario = "nievas.nahuel.1998@gmail.com";
         $cabeceras = 'From: ' . $_POST['Email'] . "\r\n" ;
-        $cabeceras .= 'Reply-To: geninfernando@hotmail.com' . "\r\n" ;
+        $cabeceras .= 'Reply-To: nievas.nahuel.1998@gmail.com' . "\r\n" ;
         if(mail($destinatario, $asunto, $mensaje, $cabeceras)){
             header('Location: Contacto.html?correo=enviado');
         } else {
